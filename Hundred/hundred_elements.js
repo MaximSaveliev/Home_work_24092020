61295a36f3469349b04f7e6ac2211aef78137722
MaximSaveliev <60519187+MaximSaveliev@users.noreply.github.com>
2020-09-27 19:57:18 +0300

    Add files via upload

---
+++ Hundred/hundred_elements.js
@@ -0,0 +1,18 @@
+for (let i=1 ; i<=10; i++){
+    document.write(`*`);
+    if(i == 10 ){
+        document.write(`<br>`);
+    }    
+}
+for (let i=1 ; i<=8; i++){
+        document.write(`*++++++++*`);
+        if(i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7 || i == 8){
+            document.write(`<br>`);
+        }
+    }
+for (let i=1 ; i<=10; i++){
+    document.write(`*`);
+    if(i == 10 ){
+        document.write(`<br>`);
+    }    
+}
